906713e731264abd9e8ad0a4b2de83a02cd97134
alberto94hdrz <78865628+alberto94hdrz@users.noreply.github.com>
2021-02-25 16:58:10 -0700

    menu

--- js/styling.js
+++ js/styling.js
@@ -11,12 +11,62 @@ $(document).mouseup(function (e) {
 // Hide navigation bar on click outside
 
 
-$(document).ready(function() {
-    $("#navbar_top").hide(); //hide your div initially
-    var topOfOthDiv = $("#about").offset().top;
-    $(window).scroll(function() {
-        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-            $("#navbar_top").show(600); //reached the desired point -- show div
-        }
-    });
+// $(document).ready(function() {
+//     $("#navbar_top").hide(); //hide your div initially
+//     var topOfOthDiv = $("#about").offset().top;
+//     $(window).scroll(function() {
+//         if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+//             $("#navbar_top").show(600); //reached the desired point -- show div
+//         }
+//     });
+// });
+
+
+
+
+
+
+// $(document).ready(function() {
+//     // $("#nav_hide").hide(); //hide your div initially
+//     var topOfOthDiv = $("#about").offset().top;
+//     $(window).scroll(function() {
+//         if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+//             $("#nav_hide").hide(600); //reached the desired point -- show div
+//         }
+//     });
+// });
+
+
+   $(window).scroll(function() {
+    var topOfOthDiv = $("#intro").offset().top;
+    if($(window).scrollTop() > topOfOthDiv) 
+     {
+        $('.a').hide(600);
+     }
+    else
+     {
+      $('.a').show(600);
+     }
+ });
+
+
+
+ $(document).ready(function() {
+    $("#navbar_top").hide(); 
 });
+
+
+
+   $(window).scroll(function() {
+    var topOfOthDiv = $("#about").offset().top;
+    if($(window).scrollTop() > topOfOthDiv) 
+     {
+        $('#navbar_top').show(600);
+     }
+     else
+     {
+        $('#navbar_top').hide(600);
+     
+     }
+ });
+
